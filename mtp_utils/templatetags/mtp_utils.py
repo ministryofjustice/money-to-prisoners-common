dad4e0ace4c589450fdf17a6f9ecdf2ff9047026
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-03-25 14:25:19 +0000

    Remove unused (and strange) template tag

--- mtp_utils/templatetags/mtp_utils.py
+++ mtp_utils/templatetags/mtp_utils.py
@@ -12,11 +12,6 @@ def to_string(value):
     return str(value)
 
 
-@register.filter(is_safe=True)
-def safewrap(val, arg):
-    return val.format(arg)
-
-
 @register.filter()
 def field_from_name(form, name):
     if name in form.fields:
