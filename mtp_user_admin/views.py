baf3e46c41b4ee80da63bbeb18977fcb9a759ae7
Ian Brechin <ian.brechin@gmail.com>
2016-03-29 14:41:12 +0100

    Add the ability to delete a user from the user management page

--- mtp_user_admin/views.py
+++ mtp_user_admin/views.py
@@ -1,10 +1,26 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import Http404
+from django.shortcuts import render
+from slumber.exceptions import HttpNotFoundError
 
 from moj_auth import api_client
 
 
 @login_required
+@permission_required('auth.change_user', raise_exception=True)
 def list_users(request):
     users = api_client.get_connection(request).users.get()['results']
-    return render_to_response('mtp_user_admin/list.html', {'users': users})
+    return render(request, 'mtp_user_admin/list.html', {'users': users})
+
+
+@login_required
+@permission_required('auth.delete_user', raise_exception=True)
+def delete_user(request, username):
+    if request.method == 'POST':
+        try:
+            api_client.get_connection(request).users(username).delete()
+            return render(request, 'mtp_user_admin/deleted.html', {'username': username})
+        except HttpNotFoundError:
+            raise Http404
+    else:
+        return render(request, 'mtp_user_admin/delete.html', {'username': username})
