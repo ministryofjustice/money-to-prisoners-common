4052a82d3437df5d1d8553cdb2250dfda65cec94
Ian Brechin <ian.brechin@gmail.com>
2016-04-01 12:39:42 +0100

    Add the ability to edit existing users

--- mtp_user_admin/forms.py
+++ mtp_user_admin/forms.py
@@ -6,37 +6,44 @@ from moj_auth import api_client
 from slumber.exceptions import HttpClientError
 
 
-class UserCreationForm(forms.Form):
+class UserUpdateForm(forms.Form):
     error_messages = {
         'generic': _('The service is currently unavailable.')
     }
 
-    username = forms.CharField()
+    username = forms.CharField(disabled=True)
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.CharField()
-    is_admin = forms.BooleanField(required=False)
+    user_admin = forms.BooleanField(required=False)
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
+        self.create = kwargs.pop('create', False)
         super().__init__(*args, **kwargs)
+        if self.create:
+            self.fields['username'].disabled = False
 
     def clean(self):
         if self.is_valid():
             data = {
-                'username': self.cleaned_data['username'],
                 'first_name': self.cleaned_data['first_name'],
                 'last_name': self.cleaned_data['last_name'],
                 'email': self.cleaned_data['email'],
-                'is_admin': self.cleaned_data['is_admin'],
+                'user_admin': self.cleaned_data['user_admin'],
             }
             try:
-                api_client.get_connection(self.request).users().post(data)
+                if self.create:
+                    data['username'] = self.cleaned_data['username']
+                    api_client.get_connection(self.request).users().post(data)
+                else:
+                    username = self.initial['username']
+                    api_client.get_connection(self.request).users(username).patch(data)
             except HttpClientError as e:
                 try:
                     response_body = json.loads(e.content.decode('utf-8'))
-                    for field in response_body['errors']:
-                        for error in response_body['errors'][field]:
+                    for field in response_body:
+                        for error in response_body[field]:
                             self.add_error(field, error)
                 except (ValueError, KeyError):
-                    raise forms.ValidationError(self.error_messages['service_unavailable'])
+                    raise forms.ValidationError(self.error_messages['generic'])
