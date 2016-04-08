0fb05c739d2f8339c5460c3f8a427cdfca8f5dd0
Ian Brechin <ian.brechin@gmail.com>
2016-04-08 16:17:27 +0100

    Add confirmation pages for user creation and editing

--- mtp_user_admin/views.py
+++ mtp_user_admin/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required, permission_required
-from django.core.urlresolvers import reverse_lazy
 from django.http import Http404
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
@@ -34,16 +33,26 @@ def delete_user(request, username):
         return render(request, 'mtp_user_admin/delete.html', {'username': username})
 
 
-@method_decorator(login_required, name='dispatch')
-@method_decorator(permission_required('auth.add_user', raise_exception=True), name='dispatch')
-class UserCreationView(FormView):
+class UserFormView(FormView):
     template_name = 'mtp_user_admin/update.html'
     form_class = UserUpdateForm
-    success_url = reverse_lazy('list-users')
 
     def get_form_kwargs(self):
         form_kwargs = super().get_form_kwargs()
         form_kwargs['request'] = self.request
+        return form_kwargs
+
+    def form_valid(self, form):
+        return render(self.request, 'mtp_user_admin/saved.html',
+                      {'user': form.cleaned_data, 'create': form.create})
+
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(permission_required('auth.add_user', raise_exception=True), name='dispatch')
+class UserCreationView(UserFormView):
+
+    def get_form_kwargs(self):
+        form_kwargs = super().get_form_kwargs()
         form_kwargs['create'] = True
         return form_kwargs
 
@@ -68,10 +77,7 @@ class UserCreationView(FormView):
 
 @method_decorator(login_required, name='dispatch')
 @method_decorator(permission_required('auth.change_user', raise_exception=True), name='dispatch')
-class UserUpdateView(FormView):
-    template_name = 'mtp_user_admin/update.html'
-    form_class = UserUpdateForm
-    success_url = reverse_lazy('list-users')
+class UserUpdateView(UserFormView):
 
     def get_initial(self):
         username = self.kwargs['username']
@@ -86,8 +92,3 @@ class UserUpdateView(FormView):
             }
         except HttpNotFoundError:
             raise Http404
-
-    def get_form_kwargs(self):
-        form_kwargs = super().get_form_kwargs()
-        form_kwargs['request'] = self.request
-        return form_kwargs
