8f7cd72808571bae4a34eca15542d80939277807
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-04-14 17:12:27 +0100

    Create shared templates for form error summaries Also, add simple field template; there's more work to be done to make it work with a variety of Django widgets

--- mtp_utils/templatetags/mtp_utils.py
+++ mtp_utils/templatetags/mtp_utils.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from django import template
 try:
     from raven.contrib.django.models import client as sentry_client
@@ -7,15 +9,32 @@ except ImportError:
 register = template.Library()
 
 
-@register.filter()
+@register.filter
 def to_string(value):
     return str(value)
 
 
-@register.filter()
+@register.filter
 def field_from_name(form, name):
     if name in form.fields:
         return form[name]
+
+
+@register.simple_tag
+def get_form_errors(form):
+    """
+    Django form errors do not obey natural field order,
+    this template tag returns non-field and field-specific errors
+    :param form: the form instance
+    """
+    return {
+        'non_field': form.non_field_errors(),
+        'field_specific': OrderedDict(
+            (field, form.errors[field.name])
+            for field in form
+            if field.name in form.errors
+        )
+    }
 
 
 @register.inclusion_tag('mtp_utils/sentry-js.html')
