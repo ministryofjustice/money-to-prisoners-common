cd8b09959bc57bdf3bc5f472713bbf28364d8d28
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-06-16 13:55:11 +0100

    Allow email sending function to override any template context defaults

--- mtp_common/email.py
+++ mtp_common/email.py
@@ -5,15 +5,17 @@ from django.core.mail import EmailMultiAlternatives
 from django.template import loader
 
 
-def send_email(to, text_template, subject, context={},
+def send_email(to, text_template, subject, context=None,
                html_template=None, from_address=None):
     if not from_address:
-        from_address = settings.MAILGUN_FROM_ADDRESS
-    if not isinstance(to, list):
+        from_address = getattr(settings, 'MAILGUN_FROM_ADDRESS', '') or settings.DEFAULT_FROM_EMAIL
+    if not isinstance(to, (list, tuple)):
         to = [to]
-    context = dict(context, static_url=urljoin(settings.SITE_URL, settings.STATIC_URL))
+    template_context = {'static_url': urljoin(settings.SITE_URL, settings.STATIC_URL)}
+    if context:
+        template_context.update(context)
 
-    text_body = loader.get_template(text_template).render(context)
+    text_body = loader.get_template(text_template).render(template_context)
     email = EmailMultiAlternatives(
         subject=subject,
         body=text_body,
@@ -21,6 +23,6 @@ def send_email(to, text_template, subject, context={},
         to=to
     )
     if html_template:
-        html_body = loader.get_template(html_template).render(context)
+        html_body = loader.get_template(html_template).render(template_context)
         email.attach_alternative(html_body, 'text/html')
     email.send()
