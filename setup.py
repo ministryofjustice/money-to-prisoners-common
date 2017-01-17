3805cd153075aa0cf44aafc459912fe445a6f9cb
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-01-17 12:56:16 +0000

    Update form error reporting library

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('README.rst') as readme:
 
 install_requires = [
     'Django>=1.9,<1.10',
-    'django-form-error-reporting>=0.4',
+    'django-form-error-reporting>=0.5',
     'django-widget-tweaks>=1.4,<1.5',
     'pytz>=2016.7',
     'requests-oauthlib>=0.6,<0.7',
