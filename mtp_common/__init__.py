987c3eade3de66727f3dbc78fbca27cc7ad09255
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-03-23 17:54:23 +0000

    Increase version to 5.9

--- mtp_common/__init__.py
+++ mtp_common/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (5, 8, 0)
+VERSION = (5, 9, 0)
 __version__ = '.'.join(map(str, VERSION))
 
 default_app_config = 'mtp_common.app.AppConfig'
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('README.rst') as readme:
 
 install_requires = [
     'Django>=1.10,<1.11',
-    'django-form-error-reporting>=0.5',
+    'django-form-error-reporting>=0.6',
     'django-widget-tweaks>=1.4,<1.5',
     'pytz>=2016.7',
     'requests-oauthlib>=0.6,<0.7',
