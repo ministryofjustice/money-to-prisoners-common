cdc52ef47cb119e9c5d983a05aa30d4ae5a114fb
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-08-09 14:51:58 +0100

    Restyle appearance of messages to improve their legibility and noticeability

--- mtp_common/assets-src/javascripts/modules/messages.js
+++ mtp_common/assets-src/javascripts/modules/messages.js
@@ -2,22 +2,7 @@
 'use strict';
 
 exports.Messages = {
-  selector: '.messages[role=alert]',
-
   init: function () {
-    this.bindEvents();
-    this.render(null, {wrap: 'body'});
-  },
-
-  bindEvents: function () {
-    $('body').on('Messages.render', $.proxy(this.render, this));
-  },
-
-  render: function (e, params) {
-    var $el = $(this.selector, $(params.wrap));
-
-    if ($el.length > 0) {
-      $el.focus();
-    }
+    $('.error-summary, .mtp-messages[role=alert]').focus();
   }
 };
