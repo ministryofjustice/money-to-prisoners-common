68020f03930b172c2fbbc787c565177e6046008a
Ian Brechin <ian.brechin@gmail.com>
2017-08-16 16:17:42 +0100

    Add support for retrieving a bare Requests session object from auth module
    This allows one to use the session object to make requests directly
    rather than going via the slumber wrapper all the time.

--- mtp_common/auth/exceptions.py
+++ mtp_common/auth/exceptions.py
@@ -1,6 +1,29 @@
-class Unauthorized(Exception):
+from requests.exceptions import HTTPError
+
+
+class ApiHttpError(HTTPError):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args)
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+
+
+class HttpClientError(ApiHttpError):
     pass
 
 
-class Forbidden(Exception):
+class Unauthorized(HttpClientError):
+    pass
+
+
+class Forbidden(HttpClientError):
+    pass
+
+
+class HttpNotFoundError(HttpClientError):
+    pass
+
+
+class HttpServerError(ApiHttpError):
     pass
