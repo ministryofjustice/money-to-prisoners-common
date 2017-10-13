6425394f569358ac6fa1cfff68fe73445252bf90
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-10-13 14:53:39 +0100

    Add JS-driven user menu to staff site headers

---
+++ mtp_common/assets-src/javascripts/modules/proposition-user-menu.js
@@ -0,0 +1,37 @@
+// Header user menu
+'use strict';
+
+exports.PropositionUserMenu = {
+  init: function () {
+    var $userNav = $('#mtp-user-menu');
+    var $linksMenu = $('#mtp-user-menu__links');
+    var $linksMenuToggle = $('.mtp-user-menu__toggle');
+
+    if (!$linksMenuToggle.length) {
+      $linksMenu.remove();
+      return;
+    }
+
+    $linksMenuToggle.attr({
+      'aria-controls': 'mtp-user-menu__links',
+      'aria-haspopup': 'true'
+    });
+    $linksMenu.attr({
+      'aria-expanded': 'false',
+      'role': 'menu'
+    });
+    $linksMenu.find('a').attr('role', 'menuitem');
+
+    var closed = true;
+    $linksMenuToggle.click(function () {
+      closed = !closed;
+      if (closed) {
+        $linksMenu.attr('aria-expanded', 'true');
+        $userNav.removeClass('mtp-user-menu--open');
+      } else {
+        $linksMenu.attr('aria-expanded', 'false');
+        $userNav.addClass('mtp-user-menu--open');
+      }
+    });
+  }
+};
