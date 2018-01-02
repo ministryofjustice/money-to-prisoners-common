4f7ab385ac3e8cda15c76f48d2e452c1b9e3ce02
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-01-02 18:17:54 +0000

    Adapt Zendesk ticket views' context data to MTP base templates

--- mtp_common/views.py
+++ mtp_common/views.py
@@ -1,4 +1,5 @@
 from django.views import defaults
+from zendesk_tickets.views import TicketView, TicketSentView
 
 
 def page_not_found(request, exception, template_name='mtp_common/errors/404.html'):
@@ -11,3 +12,23 @@ def server_error(request, template_name='mtp_common/errors/500.html'):
 
 def bad_request(request, exception, template_name='mtp_common/errors/400.html'):
     return defaults.bad_request(request, exception, template_name)
+
+
+class GetHelpView(TicketView):
+    template_name = 'mtp_common/feedback/submit_feedback.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if 'return_to' in context:
+            context['breadcrumbs_back'] = context['return_to']
+        return context
+
+
+class GetHelpSuccessView(TicketSentView):
+    template_name = 'mtp_common/feedback/success.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if 'return_to' in context:
+            context['breadcrumbs_back'] = context['return_to']
+        return context
