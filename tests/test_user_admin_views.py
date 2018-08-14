686dad57ca225b6737b77968359a5191a418ef94
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-08-14 12:07:34 +0100

    Test display of user list … and convert to using responses mock which additionally raises errors when an expected call is not made

--- tests/test_user_admin_views.py
+++ tests/test_user_admin_views.py
@@ -13,9 +13,9 @@ from .utils import SimpleTestCase
 
 
 class UserAdminTestCase(SimpleTestCase):
-    def mock_roles_list(self):
-        responses.add(
-            responses.GET,
+    def mock_roles_list(self, rsps):
+        rsps.add(
+            rsps.GET,
             urljoin(settings.API_URL, '/roles/'),
             json={
                 'count': 1,
@@ -57,80 +57,103 @@ class UserAdminTestCase(SimpleTestCase):
 
 
 class UserListTestCase(UserAdminTestCase):
+    def test_list_users(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(
+                rsps.GET,
+                urljoin(settings.API_URL, 'users'),
+                json={'results': [
+                    {
+                        'first_name': 'John', 'last_name': 'Smith',
+                        'username': 'john123', 'email': 'john@mtp.local',
+                        'is_active': True, 'is_locked_out': False, 'user_admin': False,
+                    },
+                    {
+                        'first_name': 'Mary', 'last_name': 'Marks',
+                        'username': 'mary321', 'email': 'mary@mtp.local',
+                        'is_active': False, 'is_locked_out': False, 'user_admin': True,
+                    },
+                ], 'count': 2},
+                content_type='application/json'
+            )
+            self.mocked_login()
+            response = self.client.get(reverse('list-users'))
+        content = response.content.decode(response.charset)
+        self.assertIn('john123', content)
+        self.assertIn('mary321', content)
+        self.assertIn('User can manage other accounts', content)
 
-    @responses.activate
     def test_delete_user_permission_propagates(self):
-        responses.add(
-            responses.GET,
-            urljoin(settings.API_URL, 'users'),
-            json={'results': []},
-            status=200,
-            content_type='application/json'
-        )
-        self.mocked_login()
-        response = self.client.get(reverse('list-users'))
+        with responses.RequestsMock() as rsps:
+            rsps.add(
+                rsps.GET,
+                urljoin(settings.API_URL, 'users'),
+                json={'results': []},
+                status=200,
+                content_type='application/json'
+            )
+            self.mocked_login()
+            response = self.client.get(reverse('list-users'))
         self.assertTrue(response.context['can_delete'])
 
     def test_admin_account_compatibility(self):
-        self.mocked_login(roles=['prison-clerk', 'security'])
-        response = self.client.get(reverse('list-users'))
+        with responses.RequestsMock():
+            self.mocked_login(roles=['prison-clerk', 'security'])
+            response = self.client.get(reverse('list-users'))
         self.assertEqual(response.templates[0].name, 'mtp_common/user_admin/incompatible-admin.html')
 
 
 class DeleteUserTestCase(UserAdminTestCase):
-
-    @responses.activate
     def test_user_not_found(self):
-        responses.add(
-            responses.DELETE,
-            urljoin(settings.API_URL, '/users/test123/'),
-            json={'detail': 'Not found'},
-            status=404,
-            content_type='application/json'
-        )
-        responses.add(
-            responses.GET,
-            urljoin(settings.API_URL, '/users'),
-            json={'results': [], 'count': 0},
-            status=200,
-            content_type='application/json'
-        )
+        with responses.RequestsMock() as rsps:
+            rsps.add(
+                rsps.DELETE,
+                urljoin(settings.API_URL, '/users/test123/'),
+                json={'detail': 'Not found'},
+                status=404,
+                content_type='application/json'
+            )
+            rsps.add(
+                rsps.GET,
+                urljoin(settings.API_URL, '/users'),
+                json={'results': [], 'count': 0},
+                status=200,
+                content_type='application/json'
+            )
 
-        self.mocked_login()
-        response = self.client.post(
-            reverse('delete-user', kwargs={'username': 'test123'}), follow=True)
+            self.mocked_login()
+            response = self.client.post(
+                reverse('delete-user', kwargs={'username': 'test123'}), follow=True)
         messages = response.context['messages']
         messages = [str(m) for m in messages]
         self.assertIn('Not found', messages)
 
-    @responses.activate
     def test_cannot_delete_self(self):
-        responses.add(
-            responses.DELETE,
-            urljoin(settings.API_URL, '/users/test-user/'),
-            json={'detail': 'You cannot disable yourself'},
-            status=400,
-            content_type='application/json'
-        )
-        responses.add(
-            responses.GET,
-            urljoin(settings.API_URL, '/users'),
-            json={'results': [], 'count': 0},
-            status=200,
-            content_type='application/json'
-        )
+        with responses.RequestsMock() as rsps:
+            rsps.add(
+                rsps.DELETE,
+                urljoin(settings.API_URL, '/users/test-user/'),
+                json={'detail': 'You cannot disable yourself'},
+                status=400,
+                content_type='application/json'
+            )
+            rsps.add(
+                rsps.GET,
+                urljoin(settings.API_URL, '/users'),
+                json={'results': [], 'count': 0},
+                status=200,
+                content_type='application/json'
+            )
 
-        self.mocked_login()
-        response = self.client.post(
-            reverse('delete-user', kwargs={'username': 'test-user'}), follow=True)
+            self.mocked_login()
+            response = self.client.post(
+                reverse('delete-user', kwargs={'username': 'test-user'}), follow=True)
         messages = response.context['messages']
         messages = [str(m) for m in messages]
         self.assertIn('You cannot disable yourself', messages)
 
 
 class NewUserTestCase(UserAdminTestCase):
-
-    @responses.activate
     def test_new_user(self):
         new_user_data = {
             'username': 'new_user',
@@ -140,48 +163,47 @@ class NewUserTestCase(UserAdminTestCase):
             'user_admin': False,
             'role': 'prison-clerk',
         }
+        with responses.RequestsMock() as rsps:
+            rsps.add(
+                rsps.POST,
+                urljoin(settings.API_URL, 'users'),
+                status=201,
+                content_type='application/json'
+            )
 
-        responses.add(
-            responses.POST,
-            urljoin(settings.API_URL, 'users'),
-            status=201,
-            content_type='application/json'
-        )
+            self.mocked_login()
+            self.mock_roles_list(rsps)
+            with silence_logger('mtp', level=logging.WARNING):
+                self.client.post(reverse('new-user'), data=new_user_data)
 
-        self.mocked_login()
-        self.mock_roles_list()
-        with silence_logger('mtp', level=logging.WARNING):
-            self.client.post(reverse('new-user'), data=new_user_data)
+            self.assertEqual(
+                json.loads(rsps.calls[-1].request.body.decode()),
+                new_user_data
+            )
 
-        self.assertEqual(
-            json.loads(responses.calls[-1].request.body.decode('utf-8')),
-            new_user_data
-        )
-
-    @responses.activate
     @mock.patch('tests.urls.mocked_template')
     def test_form_lists_roles(self, mocked_template):
-        mocked_template.return_value = '{{ form }}'
-        self.mocked_login()
-        self.mock_roles_list()
-        with silence_logger('mtp', level=logging.WARNING):
-            response = self.client.get(reverse('new-user'))
+        with responses.RequestsMock() as rsps:
+            mocked_template.return_value = '{{ form }}'
+            self.mocked_login()
+            self.mock_roles_list(rsps)
+            with silence_logger('mtp', level=logging.WARNING):
+                response = self.client.get(reverse('new-user'))
         self.assertContains(response, 'Digital cashbook')
         self.assertSequenceEqual(
             response.context['form']['role'].field.choices,
             [('prison-clerk', 'Digital cashbook')]
         )
 
-    @responses.activate
     def test_admin_account_compatibility(self):
-        self.mocked_login(roles=['prison-clerk', 'security'])
-        self.mock_roles_list()
-        response = self.client.get(reverse('new-user'))
+        with responses.RequestsMock():
+            self.mocked_login(roles=['prison-clerk', 'security'])
+            response = self.client.get(reverse('new-user'))
         self.assertEqual(response.templates[0].name, 'mtp_common/user_admin/incompatible-admin.html')
 
 
 class EditUserTestCase(UserAdminTestCase):
-    def _init_existing_user(self, **user_data_edits):
+    def _init_existing_user(self, rsps, **user_data_edits):
         existing_user_data = {
             'username': 'current_user',
             'first_name': 'current',
@@ -191,110 +213,107 @@ class EditUserTestCase(UserAdminTestCase):
             'roles': ['prison-clerk'],
         }
         existing_user_data.update(user_data_edits)
-        responses.add(
-            responses.GET,
+        rsps.add(
+            rsps.GET,
             urljoin(settings.API_URL, '/users/current_user/'),
             json=existing_user_data,
             status=200,
             content_type='application/json'
         )
 
-    @responses.activate
     def test_edit_user(self):
-        self._init_existing_user()
+        with responses.RequestsMock() as rsps:
+            self._init_existing_user(rsps)
 
-        self.mocked_login()
-        self.mock_roles_list()
+            self.mocked_login()
+            self.mock_roles_list(rsps)
 
-        response = self.client.get(reverse('edit-user', kwargs={'username': 'current_user'}))
-        content = response.content.decode(response.charset)
-        self.assertIn('id_user_admin', content)
-        self.assertIn('id_role', content)
-
-        updated_user_data = {
-            'first_name': 'dave',
-            'last_name': 'smith',
-            'email': 'current@user.com',
-            'user_admin': True,
-            'role': 'prison-clerk',
-        }
-        responses.add(
-            responses.PATCH,
-            urljoin(settings.API_URL, 'users/current_user/'),
-            status=204,
-            content_type='application/json'
-        )
-        with silence_logger('mtp', level=logging.WARNING):
-            self.client.post(
-                reverse('edit-user', kwargs={'username': 'current_user'}),
-                data=updated_user_data
-            )
-
-        self.assertEqual(
-            json.loads(responses.calls[-1].request.body.decode('utf-8')),
-            updated_user_data
-        )
-
-    @responses.activate
-    def test_cannot_change_username(self):
-        self._init_existing_user()
-
-        updated_user_data = {
-            'username': 'new_user_name',
-            'first_name': 'current',
-            'last_name': 'user',
-            'email': 'current@user.com',
-            'user_admin': False,
-            'role': 'prison-clerk',
-        }
-        responses.add(
-            responses.PATCH,
-            urljoin(settings.API_URL, 'users/current_user/'),
-            status=204,
-            content_type='application/json'
-        )
-
-        self.mocked_login()
-        self.mock_roles_list()
-        with silence_logger('mtp', level=logging.WARNING):
-            self.client.post(
-                reverse('edit-user', kwargs={'username': 'current_user'}),
-                data=updated_user_data
-            )
-
-        del updated_user_data['username']
-
-        self.assertEqual(
-            json.loads(responses.calls[-1].request.body.decode('utf-8')),
-            updated_user_data
-        )
-
-    @responses.activate
-    def test_editing_self_hides_roles_and_admin_status(self):
-        self._init_existing_user(username='current_user')
-        self.mocked_login(username='current_user')
-        self.mock_roles_list()
-        with silence_logger('mtp', level=logging.WARNING):
             response = self.client.get(reverse('edit-user', kwargs={'username': 'current_user'}))
+            content = response.content.decode(response.charset)
+            self.assertIn('id_user_admin', content)
+            self.assertIn('id_role', content)
+
+            updated_user_data = {
+                'first_name': 'dave',
+                'last_name': 'smith',
+                'email': 'current@user.com',
+                'user_admin': True,
+                'role': 'prison-clerk',
+            }
+
+            rsps.add(
+                rsps.PATCH,
+                urljoin(settings.API_URL, 'users/current_user/'),
+                status=204,
+                content_type='application/json'
+            )
+            with silence_logger('mtp', level=logging.WARNING):
+                self.client.post(
+                    reverse('edit-user', kwargs={'username': 'current_user'}),
+                    data=updated_user_data
+                )
+
+            self.assertEqual(
+                json.loads(rsps.calls[-1].request.body.decode()),
+                updated_user_data
+            )
+
+    def test_cannot_change_username(self):
+        with responses.RequestsMock() as rsps:
+            self._init_existing_user(rsps)
+
+            updated_user_data = {
+                'username': 'new_user_name',
+                'first_name': 'current',
+                'last_name': 'user',
+                'email': 'current@user.com',
+                'user_admin': False,
+                'role': 'prison-clerk',
+            }
+            rsps.add(
+                rsps.PATCH,
+                urljoin(settings.API_URL, 'users/current_user/'),
+                status=204,
+                content_type='application/json'
+            )
+
+            self.mocked_login()
+            self.mock_roles_list(rsps)
+            with silence_logger('mtp', level=logging.WARNING):
+                self.client.post(
+                    reverse('edit-user', kwargs={'username': 'current_user'}),
+                    data=updated_user_data
+                )
+
+            del updated_user_data['username']
+
+            self.assertEqual(
+                json.loads(rsps.calls[-1].request.body.decode()),
+                updated_user_data
+            )
+
+    def test_editing_self_hides_roles_and_admin_status(self):
+        with responses.RequestsMock() as rsps:
+            self._init_existing_user(rsps, username='current_user')
+            self.mocked_login(username='current_user')
+            with silence_logger('mtp', level=logging.WARNING):
+                response = self.client.get(reverse('edit-user', kwargs={'username': 'current_user'}))
         self.assertNotContains(response, 'Digital cashbook', msg_prefix=response.content.decode(response.charset))
         content = response.content.decode(response.charset)
         self.assertNotIn('id_user_admin', content)
         self.assertNotIn('id_role', content)
 
-    @responses.activate
     def test_admin_account_compatibility(self):
-        self._init_existing_user()
-        self.mocked_login(roles=['prison-clerk', 'security'])
-        self.mock_roles_list()
-        response = self.client.get(reverse('edit-user', kwargs={'username': 'current_user'}))
+        with responses.RequestsMock():
+            self.mocked_login(roles=['prison-clerk', 'security'])
+            response = self.client.get(reverse('edit-user', kwargs={'username': 'current_user'}))
         self.assertEqual(response.templates[0].name, 'mtp_common/user_admin/incompatible-admin.html')
 
-    @responses.activate
     def test_user_account_compatibility(self):
-        self._init_existing_user(roles=['prison-clerk', 'security'])
-        self.mocked_login()
-        self.mock_roles_list()
-        response = self.client.get(reverse('edit-user', kwargs={'username': 'current_user'}))
+        with responses.RequestsMock() as rsps:
+            self._init_existing_user(rsps, roles=['prison-clerk', 'security'])
+            self.mocked_login()
+            response = self.client.get(reverse('edit-user', kwargs={'username': 'current_user'}))
         self.assertEqual(response.templates[0].name, 'mtp_common/user_admin/incompatible-user.html')
 
 
