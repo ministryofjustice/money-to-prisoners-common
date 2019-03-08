f94c2d5437c399a082c5565c3f0a4a16dcad021f
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-03-08 16:32:41 +0000

    Allow containers in cloud platform to determine if they are the "first instance"

--- mtp_common/stack.py
+++ mtp_common/stack.py
@@ -1,5 +1,11 @@
+import os
+
 import boto3
 from botocore.exceptions import ClientError
+from django.conf import settings
+from kubernetes import client as k8s_client
+from kubernetes.client.rest import ApiException
+from kubernetes.config import ConfigException, load_incluster_config
 import requests
 
 
@@ -16,6 +22,15 @@ class InstanceNotInAsgException(StackException):
 
 
 def is_first_instance():
+    """
+    Returns True if application is "first". Used to run commands only once in load-balanced environments.
+    """
+    if os.environ.get('KUBERNETES_SERVICE_HOST'):
+        return is_first_instance_k8s()
+    return is_first_instance_aws()
+
+
+def is_first_instance_aws():
     """
     Returns True if the current instance is the first instance in the ASG group,
     sorted by instance_id.
@@ -53,4 +68,29 @@ def is_first_instance():
     except (ClientError, AssertionError) as e:
         raise StackInterrogationException(e)
 
-    return autoscaling_group_instance_ids and autoscaling_group_instance_ids[0] == instance_id
+    return bool(autoscaling_group_instance_ids and autoscaling_group_instance_ids[0] == instance_id)
+
+
+def is_first_instance_k8s(current_pod_name=None):
+    """
+    Returns True if the current pod is the first replica in Kubernetes cluster.
+    """
+    current_pod_name = current_pod_name or os.environ.get('POD_NAME')
+    if not current_pod_name:
+        raise StackInterrogationException('Pod name not known')
+
+    namespace = 'money-to-prisoners-%s' % settings.ENVIRONMENT
+    try:
+        load_incluster_config()
+    except ConfigException as e:
+        raise StackInterrogationException(e)
+    try:
+        response = k8s_client.CoreV1Api().list_namespaced_pod(
+            namespace=namespace,
+            label_selector='app=%s' % settings.APP,
+            watch=False,
+        )
+    except ApiException as e:
+        raise StackInterrogationException(e)
+    pod_names = sorted(pod.metadata.name for pod in filter(lambda pod: pod.status.phase == 'Running', response.items))
+    return bool(pod_names and pod_names[0] == current_pod_name)
