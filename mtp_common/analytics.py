ba16f8f2d057d8039de574923d1d509a9473f7ec
Marco Fucci <info@marcofucci.com>
2019-08-30 11:38:01 +0100

    Add support for GA campaigns
    This allows GA campaign settings to be sent through.

--- mtp_common/analytics.py
+++ mtp_common/analytics.py
@@ -1,18 +1,36 @@
+from urllib.parse import parse_qs
+
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.encoding import escape_uri_path
 
 
+def _get_qs_value(qs, key):
+    value = qs.get(key, [])
+    if not value:
+        return None
+    return escape_uri_path(value[0])
+
+
 def genericised_pageview(request, title=None):
-    location = request.build_absolute_uri().split('?')[0]
+    urlparts = request.build_absolute_uri().split('?', 1)
+
+    location = urlparts.pop(0)
     if location.startswith('//'):
-        location = 'https:%s' % location
-    pageview = {
+        location = f'https:{location}'
+
+    if urlparts:
+        qs = parse_qs(urlparts.pop(0))
+    else:
+        qs = {}
+
+    return {
         'page': escape_uri_path(request.path),
         'location': escape_uri_path(location),
+        'title': title,
+        'campaignName': _get_qs_value(qs, 'utm_campaign'),
+        'campaignMedium': _get_qs_value(qs, 'utm_medium'),
+        'campaignSource': _get_qs_value(qs, 'utm_source'),
     }
-    if title:
-        pageview['title'] = title
-    return pageview
 
 
 def default_genericised_pageview(request):
