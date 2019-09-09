164e47ddaf0311936d5d88e14a54c669169001a2
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-09-09 13:11:55 +0100

    Add template tag for spacing out postcodes …falls back to simply using upper case

--- mtp_common/utils.py
+++ mtp_common/utils.py
@@ -1,3 +1,5 @@
+import re
+
 from django.utils.translation import gettext
 
 
@@ -7,3 +9,10 @@ def and_join(values):
     if len(values) > 1:
         values = values[:-2] + [gettext('%s and %s') % (values[-2], values[-1])]
     return ', '.join(map(str, values))
+
+
+def format_postcode(value):
+    matches = re.match(r'^([A-Z]{1,2}\d{1,2}[A-Z]?)\s*(\d[A-Z]{2})$', value, flags=re.IGNORECASE)
+    if matches:
+        return f'{matches.group(1).upper()} {matches.group(2).upper()}'
+    return value.upper()
