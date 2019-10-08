a4a4b9077726b2d0b38c5100ee912d0bd262382d
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-10-08 17:55:16 +0100

    Add a class to store model cookie policy choices using a cookie construct similar to what GOV.UK uses

--- mtp_common/utils.py
+++ mtp_common/utils.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 from django.utils.translation import gettext
@@ -16,3 +17,28 @@ def format_postcode(value):
     if matches:
         return f'{matches.group(1).upper()} {matches.group(2).upper()}'
     return value.upper()
+
+
+class CookiePolicy:
+    """
+    A configurable cookie policy loaded from a cookie.
+    Attributes on the policy object represent different settings.
+    "usage", which is on by default, represents whether Google Analytics should be enabled.
+    Additional keys can be added when the object is instantiated,
+    but otherwise all other cookie contents are ignored.
+    """
+
+    def __init__(self, request, **defaults):
+        defaults.setdefault('usage', True)
+        cookie_policy = request.COOKIES.get('cookie_policy', '')
+        try:
+            cookie_policy = json.loads(cookie_policy)
+            if not isinstance(cookie_policy, dict):
+                raise ValueError
+            cookie_policy = {
+                policy_key: bool(cookie_policy.get(policy_key, default_value))
+                for policy_key, default_value in defaults.items()
+            }
+        except ValueError:
+            cookie_policy = defaults.copy()
+        self.__dict__ = cookie_policy
