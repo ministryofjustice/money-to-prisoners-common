4a580f5b7ccf081c5c7f11510d1620c8efc33a44
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-11-19 17:23:23 +0000

    Ensure prometheus app info metrics are not None

--- mtp_common/metrics/metrics.py
+++ mtp_common/metrics/metrics.py
@@ -6,11 +6,11 @@ from prometheus_client.metrics_core import InfoMetricFamily
 class AppMetricCollector:
     def __init__(self):
         self.info = InfoMetricFamily('mtp_app', 'Details of a money-to-prisoners app', value=dict(
-            app=getattr(settings, 'APP', 'unknown'),
-            environment=getattr(settings, 'ENVIRONMENT', 'local'),
-            git_commit=getattr(settings, 'APP_GIT_COMMIT', 'unknown'),
-            build_tag=getattr(settings, 'APP_BUILD_TAG', 'unknown'),
-            build_date=getattr(settings, 'APP_BUILD_DATE', 'unknown'),
+            app=getattr(settings, 'APP', None) or 'unknown',
+            environment=getattr(settings, 'ENVIRONMENT', None) or 'local',
+            git_commit=getattr(settings, 'APP_GIT_COMMIT', None) or 'unknown',
+            build_tag=getattr(settings, 'APP_BUILD_TAG', None) or 'unknown',
+            build_date=getattr(settings, 'APP_BUILD_DATE', None) or 'unknown',
         ))
 
     def collect(self):
