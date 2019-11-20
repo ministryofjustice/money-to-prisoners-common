e48fe46dd8d29fbe2e978c516f4e39fccec7e794
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-11-20 13:34:28 +0000

    Ensure prometheus metrics app is always available

--- mtp_common/metrics/views.py
+++ mtp_common/metrics/views.py
@@ -1,13 +1,9 @@
 from django.apps import apps
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
+from prometheus_client import exposition
 
 
 def metrics_view(request):
-    try:
-        from prometheus_client import exposition
-    except ImportError:
-        raise Http404()
-
     app = apps.get_app_config('metrics')
     encoder, content_type = exposition.choose_encoder(request.META.get('HTTP_ACCEPT'))
     content = encoder(app.metric_registry)
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,6 +1,3 @@
-import warnings
-
-from django.http import Http404
 from django.test import RequestFactory
 
 from mtp_common.metrics.views import metrics_view
@@ -10,12 +7,6 @@ from tests.utils import SimpleTestCase
 class MetricsTestCase(SimpleTestCase):
     def test_app_info_metrics(self):
         request = RequestFactory().get('/metrics/')
-        try:
-            import prometheus_client  # noqa: F401
-            response = metrics_view(request)
-            self.assertContains(response, 'app="common"')
-            self.assertContains(response, 'environment="test"')
-        except ImportError:
-            warnings.warn('prometheus_client not installed')
-            with self.assertRaises(Http404):
-                metrics_view(request)
+        response = metrics_view(request)
+        self.assertContains(response, 'app="common"')
+        self.assertContains(response, 'environment="test"')
