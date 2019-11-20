d8d2ba5600a9aed71fa6aba9da2797d918574750
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-11-20 18:26:31 +0000

    Add enforced basic-auth check to prometheus metrics

--- mtp_common/metrics/views.py
+++ mtp_common/metrics/views.py
@@ -2,7 +2,10 @@ from django.apps import apps
 from django.http import HttpResponse
 from prometheus_client import exposition
 
+from mtp_common.auth.basic import basic_auth
 
+
+@basic_auth('METRICS_USER', 'METRICS_PASS')
 def metrics_view(request):
     app = apps.get_app_config('metrics')
     encoder, content_type = exposition.choose_encoder(request.META.get('HTTP_ACCEPT'))
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,9 +1,13 @@
-from django.test import RequestFactory
+from unittest import mock
+
+from django.test import RequestFactory, override_settings
 
 from mtp_common.metrics.views import metrics_view
 from tests.utils import SimpleTestCase
 
 
+@mock.patch('mtp_common.auth.basic._check_basic_auth', lambda *args: True)
+@override_settings(METRICS_USER='???', METRICS_PASS='???')
 class MetricsTestCase(SimpleTestCase):
     def test_app_info_metrics(self):
         request = RequestFactory().get('/metrics/')
