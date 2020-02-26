12f09f9e521ac3174fe5d0c5f4bcca100dc5cfd6
Igor <igor.ushkarev@digital.justice.gov.uk>
2020-02-26 18:09:59 +0000

    Build webpack config with consistent absolute paths

--- mtp_common/templates/mtp_common/build_tasks/webpack.config.js
+++ mtp_common/templates/mtp_common/build_tasks/webpack.config.js
@@ -4,16 +4,16 @@ const webpack = require('webpack');
 
 module.exports = {
   mode: 'none',  // overridden with 'production' when app Docker images are built
-  entry: './{{ app.javascript_source_path }}/app.js',
+  entry: '{{ app.root_path }}/{{ app.javascript_source_path }}/app.js',
   output: {
-    path: __dirname + '/{{ app.javascript_build_path }}',
+    path: '{{ app.root_path }}/{{ app.javascript_build_path }}',
     filename: 'app.js'
   },
   resolve: {
     modules: [
-      __dirname + '/node_modules',
+      '{{ app.root_path }}/node_modules',
       {% for path in app.javascript_include_paths %}
-        '{{ path }}'{% if not forloop.last %},{% endif %}
+        '{{ app.root_path }}/{{ path }}'{% if not forloop.last %},{% endif %}
       {% endfor %}
     ]
   },
