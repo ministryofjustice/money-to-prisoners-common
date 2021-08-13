20d955a2b552fe5826b2dd4c93115acc1b0cf8fd
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-08-13 10:48:32 +0100

    Add a test utility to mock GOV.UK Notify api …for use in tests whose side effect is sending an email

--- mtp_common/test_utils/notify.py
+++ mtp_common/test_utils/notify.py
@@ -1,9 +1,11 @@
+import json
 import uuid
 
 from django.test import SimpleTestCase
 import responses
 
 from mtp_common.notify import NotifyClient
+from mtp_common.notify.templates import NotifyTemplateRegistry
 
 GOVUK_NOTIFY_API_BASE_URL = 'https://api.notifications.service.gov.uk'
 GOVUK_NOTIFY_TEST_API_KEY = 'test-11111111-1111-1111-1111-111111111111-22222222-2222-2222-2222-222222222222'
@@ -22,6 +24,85 @@ class NotifyBaseTestCase(SimpleTestCase):
         NotifyClient.shared_client.cache_clear()
 
 
+class NotifyMock(responses.RequestsMock):
+    """
+    Used for testing sending of emails from MTP apps.
+    Intercepts all calls to send emails to templates registered with NotifyTemplateRegistry.
+    Assertions can be made on `send_email_calls` before the context manager exits.
+    """
+
+    def __init__(self, assert_all_requests_are_fired=False, **kwargs):
+        super().__init__(assert_all_requests_are_fired=assert_all_requests_are_fired, **kwargs)
+
+    def __enter__(self):
+        # ensure client is not reused
+        NotifyClient.shared_client.cache_clear()
+
+        super().__enter__()
+
+        # return all registered templates
+        mock_all_templates_response(self, templates=[
+            dict(
+                _base_email_template_response,
+                id=str(uuid.uuid4()),
+                name=template_name,
+                subject=template_details['subject'],
+                body=template_details['body'],
+                personalisation={
+                    field: {'required': True}
+                    for field in template_details['personalisation']
+                },
+            )
+            for template_name, template_details in NotifyTemplateRegistry.get_all_templates().items()
+        ])
+
+        def send_email_callback(request):
+            request_data = json.loads(request.body)
+            template_id = request_data['template_id']
+            reference = request_data.get('reference')
+            response_data = fake_email_response(template_id, reference=reference)
+            return 200, {}, json.dumps(response_data)
+
+        # allow sending emails without requiring specific params so assertions can be made by specific tests
+        self.add_callback(
+            responses.POST,
+            f'{GOVUK_NOTIFY_API_BASE_URL}/v2/notifications/email',
+            callback=send_email_callback,
+            content_type='application/json',
+        )
+
+        return self
+
+    @property
+    def send_email_calls(self):
+        """
+        Filter out requests calls only to send emails
+        """
+
+        def send_email_filter(call):
+            return call.request.url == f'{GOVUK_NOTIFY_API_BASE_URL}/v2/notifications/email' \
+                   and call.request.method == responses.POST
+
+        return list(filter(send_email_filter, self.calls))
+
+    @property
+    def send_email_request_data(self):
+        """
+        Returns only the request data posted whilst sending emails
+        """
+        return [
+            json.loads(call.request.body)
+            for call in self.send_email_calls
+        ]
+
+
+_base_email_template_response = {
+    'type': 'email', 'postage': None, 'letter_contact_block': None,
+    'created_at': '2021-08-11T11:00:00Z', 'updated_at': '2021-08-11T11:00:00Z', 'created_by': 'mtp@localhost',
+    'version': 1,
+}
+
+
 def fake_template(template_id, template_name, required_personalisations=()):
     # fakes a template details response from GOV.UK Notify
     # the template called "generic" is special and is always expected to exist
@@ -30,19 +111,17 @@ def fake_template(template_id, template_name, required_personalisations=()):
         f'(({field}))'
         for field in required_personalisations
     )
-    return {
-        'type': 'email', 'postage': None, 'letter_contact_block': None,
-        'created_at': '2021-08-11T11:00:00Z', 'updated_at': '2021-08-11T11:00:00Z', 'created_by': 'mtp@localhost',
-        'version': 1,
-        'id': template_id,
-        'name': template_name,
-        'subject': subject,
-        'body': body,
-        'personalisation': {
+    return dict(
+        _base_email_template_response,
+        id=template_id,
+        name=template_name,
+        subject=subject,
+        body=body,
+        personalisation={
             field: {'required': True}
             for field in required_personalisations
         },
-    }
+    )
 
 
 def mock_all_templates_response(rsps, templates=()):
@@ -58,22 +137,9 @@ def mock_all_templates_response(rsps, templates=()):
     )
 
 
-def mock_send_email_response(rsps, template_id, to, personalisation=None, reference=None, staff_email=None):
-    json_sent = {
-        'template_id': template_id,
-        'email_address': to,
-    }
-    if personalisation:
-        json_sent['personalisation'] = personalisation
-    if reference:
-        json_sent['reference'] = reference
-    if staff_email is True:
-        json_sent['email_reply_to_id'] = GOVUK_NOTIFY_TEST_REPLY_TO_STAFF
-    if staff_email is False:
-        json_sent['email_reply_to_id'] = GOVUK_NOTIFY_TEST_REPLY_TO_PUBLIC
-
+def fake_email_response(template_id, reference=None):
     message_id = str(uuid.uuid4())
-    json_received = {
+    return {
         'id': message_id, 'reference': reference,
         'uri': f'{GOVUK_NOTIFY_API_BASE_URL}/v2/notifications/{message_id}',
         'template': {
@@ -90,10 +156,25 @@ def mock_send_email_response(rsps, template_id, to, personalisation=None, refere
         'scheduled_for': None,
     }
 
+
+def mock_send_email_response(rsps, template_id, to, personalisation=None, reference=None, staff_email=None):
+    json_sent = {
+        'template_id': template_id,
+        'email_address': to,
+    }
+    if personalisation:
+        json_sent['personalisation'] = personalisation
+    if reference:
+        json_sent['reference'] = reference
+    if staff_email is True:
+        json_sent['email_reply_to_id'] = GOVUK_NOTIFY_TEST_REPLY_TO_STAFF
+    if staff_email is False:
+        json_sent['email_reply_to_id'] = GOVUK_NOTIFY_TEST_REPLY_TO_PUBLIC
+
     rsps.add(
         responses.POST,
         f'{GOVUK_NOTIFY_API_BASE_URL}/v2/notifications/email',
         match_querystring=True,
-        json=json_received,
+        json=fake_email_response(template_id, reference=reference),
         match=[responses.json_params_matcher(json_sent)],
     )
