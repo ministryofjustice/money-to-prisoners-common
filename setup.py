afdebd895b7efbed42951542eb5923ffbe2dbb73
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-04-23 11:51:14 +0100

    Update python dependencies

--- setup.py
+++ setup.py
@@ -17,41 +17,41 @@ with open('README.rst') as readme:
 
 install_requires = [
     # third-party dependencies (versions should be flexible to allow for bug fixes)
-    'Django>=2.2,<2.3',
+    'Django>=2.2,<2.3',  # mtp apps are only compatible with django 2.2
     'django-extended-choices>=1.3,<2',
     'django-widget-tweaks>=1.4,<1.5',
-    'notifications-python-client~=6.2',
-    'pytz>=2021.1',
-    'requests>=2.18,<3',
+    'notifications-python-client~=6.3',
+    'pytz>=2022.1',
+    'requests>=2.27,<3',
     'requests-oauthlib>=1,<2',
     'slumber>=0.7,<0.8',
     'selenium~=4.0',
     'transifex-client>=0.14,<0.15',
     'cryptography>=36,<37',
     'boto3>=1.18.32,<1.21.0',
-    'kubernetes~=19.0',  # corresponds to server version 1.19 (matching `live` cluster)
-    'prometheus_client>=0.6,<1',
-    'sentry-sdk~=1.4',
-    'libsass~=0.20',
+    'kubernetes~=21.0',  # corresponds to server version 1.21 (matching `live` cluster)
+    'prometheus-client>=0.14,<1',
+    'sentry-sdk~=1.5',
+    'libsass~=0.21',
     'uWSGI~=2.0.20',
 
     # moj-built dependencies (should be locked versions)
-    'django-form-error-reporting==0.9',
-    'django-moj-irat==0.6',
-    'django-zendesk-tickets==0.14',
-    'govuk-bank-holidays==0.10',
+    'django-form-error-reporting==0.10',
+    'django-moj-irat==0.7',
+    'django-zendesk-tickets==0.15',
+    'govuk-bank-holidays==0.11',
 ]
 extras_require = {
     'testing': [
         # third-party dependencies (versions should be flexible to allow for bug fixes)
         'flake8~=4.0',
-        'flake8-blind-except~=0.2.0',
-        'flake8-bugbear~=21.0',
+        'flake8-blind-except~=0.2.1',
+        'flake8-bugbear~=22.0',
         'flake8-debugger~=4.0',
-        'flake8-quotes~=3.3.0',
+        'flake8-quotes~=3.3.1',
         'pep8-naming~=0.12.0',
-        'responses~=0.16.0',
-        'twine~=3.4',
+        'responses~=0.20.0',
+        'twine~=4.0',
         'watchdog~=2.1',
     ],
 }
